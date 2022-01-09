4780f843fb420e5f03fce162e026aec95e1eb1eb
Ryosuke Ito <rito.0305@gmail.com>
2022-01-10 08:23:49 +0900

    Remove unnecessary future imports

--- tests/test_backquotes.py
+++ tests/test_backquotes.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from __future__ import with_statement
-
 import distutils.version
 import os
 import subprocess
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,7 +2,6 @@
 # -*- coding:utf-8 -*-
 
 from __future__ import unicode_literals
-from __future__ import with_statement
 
 import contextlib
 import imp
